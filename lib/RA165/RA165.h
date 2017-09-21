ac4cd25c1decb0e9b9ae1a0fada6a0626dd171d0
Michael Romans <mromans@localhost.localdomain>
2017-09-21 13:10:36 -0500

    Docs in the header

--- lib/RA165/RA165.h
+++ lib/RA165/RA165.h
@@ -2,13 +2,23 @@
 #ifdef RA165_h
 #include <Arduino.h>
 
+/*
+by Mike Romans of RomansAudio
+
+Simple Oop class for the 165 shiftin register.
+*/
+
 enum RAORDER {LSB, MSB};
 
 class RA165 {
     public:
+        // Setup the object with the clock and data pins
         RA165(int clockPin, int dataPin);
+        // Read a byte from the register
         byte readByte(RAORDER byteOrder);
+        // Read the byte from the register to the internal byte
         void read(RAORDER byteOrder);
+        // Get a bit from the interal byte after a read
         int getBit(int bitToGet);
     protected:
         int             _clockPin;
